d144f8b292f4180c2cdd739bd7120eeb9e5b9a28
Liza <lizaamatya@gmail.com>
2020-01-08 14:08:09 +0545

    Updates

---
+++ precache-manifest.139668ce9addd352b6f28c28797bc6f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b12822149800e6e8446d6c68990873",
+    "url": "/contacts-ui/index.html"
+  },
+  {
+    "revision": "b316288227f6923190b2",
+    "url": "/contacts-ui/static/css/main.8cec2889.chunk.css"
+  },
+  {
+    "revision": "66f9333979328dad308e",
+    "url": "/contacts-ui/static/js/2.4cf77bc4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/contacts-ui/static/js/2.4cf77bc4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b316288227f6923190b2",
+    "url": "/contacts-ui/static/js/main.dbb4deeb.chunk.js"
+  },
+  {
+    "revision": "ac63b9c9c5f2989c48b5",
+    "url": "/contacts-ui/static/js/runtime-main.5810b799.js"
+  }
+]);
